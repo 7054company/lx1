62492d4f596f47e022097d281ca21d836640b4d5
GitPusher <7054company@gmail.com>
2025-07-09 15:01:19 +0530

    Update files via GitPusher

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Secure Iframe Loader</title>
+      <style>
+        html, body, iframe {
+          margin: 0;
+          padding: 0;
+          height: 100%;
+          width: 100%;
+          border: none;
+        }
+      </style>
+    </head>
+    <body>
+      <iframe
+        src="https://jazzy-centaur-f43e76.netlify.app"
+        allow="fullscreen; clipboard-read; clipboard-write; geolocation; microphone; camera; display-capture; autoplay"
+        sandbox="allow-same-origin allow-scripts allow-forms allow-downloads allow-modals allow-popups allow-popups-to-escape-sandbox allow-presentation allow-top-navigation"
+        style="width:100%; height:100%; border:none;"
+      ></iframe>
+    </body>
+    </html>
+  `);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
